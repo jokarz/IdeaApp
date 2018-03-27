6892e57ad8f09fd12c7012ee9a7b73cf0293c3e6
Tan Peng Chong <pctan.2016@sis.smu.edu.sg>
2018-03-27 15:03:05 +0800

    Added Idea Model

---
+++ IdeaApp/Models/Idea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeaApi.Models
+{
+    public class Idea
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; } // 0 = pending, -1 = rejected, 1 = accepted
+    }
+}
